4eafffbeb05aace7707578e945f7ba1c9201da17
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-02-16 22:20:35 -0200

    add images to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+  validates_presence_of :name, :type_kitchen, :preference, :serves_people, :time_preper, :level, :ingredients, :steps, :image
+  has_attached_file :image, :styles => { :large => "600x00>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
---
+++ db/migrate/20150216193615_add_image_to_recipes.rb
@@ -0,0 +1,11 @@
+class AddImageToRecipes < ActiveRecord::Migration
+  def self.up
+    change_table :recipes do |t|
+      t.attachment :image
+    end
+  end
+  
+  def self.down
+    remove_attachment :recipes, :image
+  end
+end
