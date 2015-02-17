00abe4323e51007a2098027aa9b01a0388256e49
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-02-17 00:38:18 -0200

    add users / merge branch

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def update
+  end
+
+  def create
+     @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :cidade, :email, :facebook, :twitter, :favorite_cuisine) 
+  end
+end
