a1ba772e3b3ae6631986eb221cb7c86aadab457c
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-02-18 23:25:37 -0200

    install devise for users and admins

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,37 +1,39 @@
 class RecipesController < ApplicationController
-  def index
-    @recipes = Recipe.all
-  end
+before_action :authenticate_user!, only: [:create]
 
-  def new
-    @recipe = Recipe.new
-  end
+def index
+  @recipes = Recipe.all
+end
 
-  def create
-    @recipe = Recipe.new(recipe_params)
-    if @recipe.save 
-      redirect_to [@recipe]
-    else
-      render "new"
-    end
-  end
+def new
+  @recipe = Recipe.new
+end
 
-  def show
-    @recipe = Recipe.find(params[:id])
+def create
+  @recipe = Recipe.new(recipe_params)
+  if @recipe.save 
+    redirect_to [@recipe]
+  else
+    render "new"
   end
+end
 
-  def edit
-    @recipe = Recipe.find(params[:id])
-  end
+def show
+  @recipe = Recipe.find(params[:id])
+end
 
-  def update
-    @recipe = Recipe.find(params[:id])
-    if @recipe.update(recipe_params)
-    redirect_to @recipe
-    else
-      render "edit"
-    end
+def edit
+  @recipe = Recipe.find(params[:id])
+end
+
+def update
+  @recipe = Recipe.find(params[:id])
+  if @recipe.update(recipe_params)
+  redirect_to @recipe
+  else
+    render "edit"
   end
+end
 
 private
   def recipe_params
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,6 @@
-class User < ActiveRecord::Base
+class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates_presence_of :name, :cidade, :email, :facebook, :twitter, :favorite_cuisine
-  end
+end
